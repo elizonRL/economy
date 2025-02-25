0f7ca0fa816d30a24b257a1fbfbf6663e5df6031
Elizon Rodrigues Liriano <118481749+elizonRL@users.noreply.github.com>
2025-02-25 10:12:40 -0400

    Agregar supertest como dependencia de desarrollo y actualizar el script de prueba en package.json; crear archivo de controlador para el servicio de economía

---
+++ src/Controller/Economy.comtroller.ts
@@ -0,0 +1,14 @@
+/*
+import { Response } from 'express'
+import { Economy } from '../utils/types'
+import economySevice from '../services/economy.sevice'
+
+const getMethodAllEconomy = (req: Request, res: Response) => {
+  const economy = economySevice.getEconomy()
+  res.send(economy)
+}
+
+export default {
+  getMethodAllEconomy
+}
+ */
